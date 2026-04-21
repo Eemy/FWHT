22b6bba2e7c29ee59bb310d5875c1e565e78e505
Emily Yang <u0835353@utah.edu>
2026-04-21 16:49:52 -0600

    HOME BASE: finally was able to reproduce some of the trends from the original spreadsheet

---
+++ src/2026_04_21/paper_results.m
@@ -0,0 +1,73 @@
+axisFontSize = 10; %originally 20
+titleFontSize = 10; %originally 25
+legendFontSize = 10; %originally 20
+
+minComponents = 2;
+maxComponents = 10;
+numComponents = minComponents:maxComponents;
+numFiles = 20; 
+
+feMethods = ["HT","WHT"];
+%threshMethods = ["Linear (FP)", "Bin (FP)", "Bin (8-Bit)"];
+FE_legend = ["FE=2","FE=3","FE=4","FE=5","FE=6","FE=7","FE=8","FE=9","FE=10"];
+
+%AVG = zeros(maxComponents-minComponents+1,length(trainSizes),length(feMethods));
+%STD = zeros(length(threshMethods),length(trainSizes),length(feMethods));
+
+%directories = ["HT_slidingWindow/","WHT_slidingWindow/"];
+%matFiles = ["FSDE_fsa_linear.mat","FSDE_fsa_bin.mat","FSDE_fsa_bin_quant.mat"; ...
+%    "PCA_FD_fsa_linear.mat","PCA_FD_fsa_bin.mat","PCA_FD_fsa_bin_quant.mat"];
+
+%lowNoiseIndices = [1,3,5,7];
+%highNoiseIndices = [2,4,6,8];
+
+suffix = "_slidingWindow_all_Euclidean_kmeans.mat";
+
+%% Consolidate Data
+CHT = zeros(numFiles,length(FE_legend));
+WHT = zeros(numFiles,length(FE_legend));
+for i=1:length(feMethods)
+    if i==1
+        feString = "HT";
+        feMat = CHT;
+    else
+        feString = "FWHT";
+        feMat = WHT;
+    end
+
+    for j = 1:length(numComponents)
+        fileName = strcat(feString+numComponents(j),suffix);
+        load(fileName);
+        feMat(:,j) = avgFScoreUnseen;
+    end
+    
+    if i==1
+        CHT = feMat;
+    else
+        WHT = feMat;
+    end
+end
+
+%% Plot Parameters
+hues = linspace(0,1,length(FE_legend)+1); hues(end) = [];  % remove redundant last hue = 1
+colors = zeros(length(FE_legend),3);
+for colorIdx=1:length(FE_legend)
+    colors(colorIdx,:) = hsv2rgb([hues(colorIdx),1,0.8]); 
+end
+
+%% Generate Plots
+classificationResults = figure('Name',"f1scores");     
+for fileIdx = 1:numFiles
+    subplot(5,4,fileIdx)
+    colororder(colors);
+
+    FScore_results = [CHT(fileIdx,:) ; WHT(fileIdx,:)];
+    hb = bar(FScore_results);
+    set(gca,'xticklabel',feMethods);
+    ylabel("F-Score","FontSize",10);
+    ylim([50,100]);
+    title(baseName(fileIdx));
+end
+sgtitle("Sliding Consecutive Coefficients");
+set(classificationResults,"Position",[0,0,1880,1250]);
+imwrite(getframe(classificationResults).cdata,strcat("ht_wht_",get(classificationResults,'Name'),".png"));
